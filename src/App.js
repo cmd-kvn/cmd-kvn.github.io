f1e589cb5a421dc44b506b20f16c2d70f37e12bc
cmd-kvn <kvn.m.wong@gmail.com>
2017-05-18 17:42:08 -0700

    App uses state handlers create to change state of background and headshot

--- src/App.js
+++ src/App.js
@@ -9,28 +9,32 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            src: 'headshot.JPG',
+            src: 'headshot_framed.jpg',
+            background: 'none'
         };
         this.handleMouseLeave = this.handleMouseLeave.bind(this);
         this.handleMouseOver = this.handleMouseOver.bind(this);
     }
 
     handleMouseOver() {
-        this.setState({ src: 'email-icon.png' });
+        this.setState({ src: 'comic_framed.jpg', background: 'background.gif' });
     }
+
     handleMouseLeave() {
-        this.setState({ src: 'headshot.JPG' });
+        this.setState({ src: 'headshot_framed.jpg', background: 'none' });
     }
 
     render() {
+        const style = { backgroundImage: `url(${this.state.background})` };
+
         return (
-            <div className='App'>
-                <Aside 
-                    picture={this.state.src} 
-                    handleMouseLeave={this.handleMouseLeave} 
-                    handleMouseOver={this.handleMouseOver} 
-                />
-                <Main />
+            <div className='App' style={style}>
+                    <Aside 
+                        picture={this.state.src} 
+                        handleMouseLeave={this.handleMouseLeave} 
+                        handleMouseOver={this.handleMouseOver} 
+                    />
+                    <Main />
             </div>
         );
     }
